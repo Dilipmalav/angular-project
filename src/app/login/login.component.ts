2bf2dbd142b783102f5d46fbafce125b39987dbd
YourUserName <dilipmalav8@gmail.com>
2025-01-10 01:13:05 +0530

    done

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  form: any = {
+    data: {},
+    message: '',
+    inputerror: {}
+  }
+
+constructor( private router :Router,private httpClient: HttpClient){
+
+}
+
+signIn(){
+
+  this.httpClient.post('http://localhost:8080/Auth/login', this.form.data).subscribe((res: any) =>{
+
+  console.log('res =>', res)
+
+  this.form.message = '';
+  this.form.inputerror = {};
+
+  if (res.result.message) {
+    this.form.message = res.result.message;
+  }
+  if (!res.success) {
+    this.form.inputerror = res.result.inputerror;
+  }
+
+  if (res.success && res.result.data != null) {
+    this.router.navigateByUrl('welcome');
+  }
+
+  })
+
+}
+
+signUp() {
+  this.router.navigateByUrl('signup');
+}
+
+}
